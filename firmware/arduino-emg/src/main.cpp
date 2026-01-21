fd876ed6b4858b75bc540a25b92ae1ee4040559b
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-21 17:14:41 -0300

    feat(arduino): loop now uses emg::Reader

--- firmware/arduino-emg/src/main.cpp
+++ firmware/arduino-emg/src/main.cpp
@@ -1,35 +1,26 @@
-// Simple EMG plotter
-// Usar con: pio device monitor (o Serial Plotter del Arduino IDE)
-
 #include "HardwareSerial.h"
+#include "emg_reader.h"
 #include <Arduino.h>
 
 constexpr uint8_t EMG_PIN_0 = A0;
 constexpr uint8_t EMG_PIN_1 = A1;
 constexpr uint16_t SAMPLE_RATE_HZ = 5;
-constexpr uint32_t SAMPLE_INTERVAL_US = 1000000 / SAMPLE_RATE_HZ;  // 500000 us = 0.5 seg
+constexpr uint32_t SAMPLE_INTERVAL_US =
+    1000000 / SAMPLE_RATE_HZ; // 500000 us = 0.5 seg
+
+constexpr uint32_t baudrate = 115200;
+
+static emg::Reader reader;
 
 uint32_t last_sample_us = 0;
 
-void setup() {
-    Serial.begin(115200);
-    pinMode(EMG_PIN_0, INPUT);
-    pinMode(EMG_PIN_1, INPUT);
-}
+void setup() { Serial.begin(baudrate); }
 
 // We are detecting low peaks while user has tense muscle
 void loop() {
-    uint32_t now = micros();
-    
-    if (now - last_sample_us >= SAMPLE_INTERVAL_US) {
-        last_sample_us = now;
-
-        uint16_t ch0 = analogRead(EMG_PIN_0);
-        uint16_t ch1 = analogRead(EMG_PIN_1);
-      
-        Serial.print("A0: ");
-        Serial.println(ch0);
-        Serial.print("A1: ");
-        Serial.println(ch1);
-    }
+  uint32_t now = micros();
+  if (now - last_sample_us >= SAMPLE_INTERVAL_US) {
+    last_sample_us = now;
+    reader.read_sample();
+  }
 }
