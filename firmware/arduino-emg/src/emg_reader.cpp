d723c55499c86870f67a4e81aa703c30675a57ea
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-21 14:18:56 -0300

    fix(emg): working on reader

--- firmware/arduino-emg/src/emg_reader.cpp
+++ firmware/arduino-emg/src/emg_reader.cpp
@@ -1,2 +1,18 @@
 // TODO: Implementar
 #include "emg_reader.h"
+#include "Arduino.h"
+
+emg::Reader::Reader(const Config &config) {
+  this->config = config;
+  // Init INPUT pinModes for every muscle
+  for (uint8_t i = A0; i < A0 + emg::MUSCLES - 1; i++)
+    pinMode(i, INPUT);
+}
+
+emg::EmgSample emg::Reader::read_sample() {
+  emg::EmgSample sample;
+  for (uint8_t i = A0; i < A0 + emg::MUSCLES - 1; i++)
+    sample.channels[i] = analogRead(i);
+  return sample;
+}
+
--- firmware/arduino-emg/src/emg_reader.h
+++ firmware/arduino-emg/src/emg_reader.h
@@ -1,38 +1,56 @@
 #pragma once
 
+#include "Arduino.h"
+#include "HardwareSerial.h"
 #include <stdint.h>
 
 namespace emg {
 
-// Configuracion de canales EMG
-constexpr uint8_t MAX_CHANNELS = 2;
+constexpr uint8_t MUSCLES = 2;
+constexpr uint8_t MAX_CHANNELS = 6;
 constexpr uint16_t DEFAULT_SAMPLE_RATE_HZ = 500;
 
-struct EmgSample {
-    uint32_t timestamp_ms;
-    uint16_t channels[MAX_CHANNELS];
-    uint8_t num_channels;
-};
-
 struct Config {
-    uint8_t pin_channels[MAX_CHANNELS];  // Pines analogicos
-    uint8_t num_channels;
-    uint16_t sample_rate_hz;
+  uint8_t pin_channels[MAX_CHANNELS];
+  uint8_t num_channels;
+  uint16_t sample_rate_hz;
+
+  Config()
+      : pin_channels{0}, num_channels(emg::MAX_CHANNELS),
+        sample_rate_hz(emg::DEFAULT_SAMPLE_RATE_HZ) {};
+
+  Config(uint8_t num_channels, uint16_t sample_rate_hz)
+      : pin_channels{0}, num_channels(num_channels),
+        sample_rate_hz(sample_rate_hz) {};
 };
 
-// Inicializa el lector con configuracion
-void init(const Config& config);
+struct EmgSample {
+  uint32_t timestamp_ms;
+  uint16_t channels[MAX_CHANNELS];
+  uint8_t num_channels;
+};
 
-// Lee una muestra de todos los canales
-EmgSample read_sample();
+class Reader {
+private:
+  Config config;
 
-// Obtiene el valor RMS de un canal (ventana de N muestras)
-uint16_t get_rms(uint8_t channel);
+public:
+  Reader(const Config &config);
 
-// Detecta si hay activacion muscular en un canal
-bool is_muscle_active(uint8_t channel, uint16_t threshold);
+  ~Reader() = default;
 
-// Obtiene la configuracion actual
-const Config& get_config();
+  void init(const Config &config);
+
+  // Lee una muestra de todos los canales
+  EmgSample read_sample();
+
+  // Obtiene el valor RMS de un canal (ventana de N muestras)
+  uint16_t get_rms(uint8_t channel);
+
+  // Detecta si hay activacion muscular en un canal
+  bool is_muscle_active(uint8_t channel, uint16_t threshold);
+
+  const Config &get_config() { return this->config; }
+};
 
 } // namespace emg
