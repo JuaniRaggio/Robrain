58cec3d9078956ebd3d98470379e39e0b7d142d1
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-19 01:24:22 -0300

    fix: serial protocol

--- firmware/arduino-emg/src/serial_protocol.h
+++ firmware/arduino-emg/src/serial_protocol.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "HardwareSerial.h"
 #include <emg.h>
 #include <stdint.h>
 
@@ -18,21 +17,31 @@ constexpr const uint8_t max_payload_size = single_muscle_payload_size * 2;
 struct Payload {
   uint8_t leftBicep[single_muscle_payload_size];
   uint8_t rightBicep[single_muscle_payload_size];
-} __attribute__((packed));
 
-// TODO Agregarle metodos al packet para que esten linkeados al mismo,
-// como default initializers para que se haga el checksum automaticamente,
-// setter para el payload, etc.
-struct Packet {
-  uint8_t b_start;
-  uint8_t type;
-  uint8_t b_size;
-  Payload payload;
-  uint8_t check_sum;
-  uint8_t b_end;
+  uint8_t get_sum() const;
 } __attribute__((packed));
 
 // START(1) + TYPE(1) + LEN(1) + PAYLOAD(N) + CHECKSUM(1) + END(1)
+struct Packet {
+  const uint8_t b_start;
+  const uint8_t type;
+  const uint8_t b_size;
+  Payload payload;
+  uint8_t check_sum;
+  const uint8_t b_end;
+
+  Packet();
+  Packet *const compute_checksum();
+  template <typename Filler> Packet *const fill_payload(Filler filler_function);
+  size_t send() const;
+} __attribute__((packed));
+
+template <typename Filler>
+Packet *const Packet::fill_payload(Filler filler_function) {
+  filler_function(emg::Muscle::LeftBicep, this->payload.leftBicep);
+  filler_function(emg::Muscle::RightBicep, this->payload.rightBicep);
+  return this;
+}
 
 enum class MessageType : uint8_t {
   emgLeftBicep = static_cast<uint8_t>(emg::Muscle::LeftBicep),
