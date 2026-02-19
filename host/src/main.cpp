4d292e04db31b7aff25f32088ca88f4507f5271b
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-19 16:26:26 -0300

    feat: starting host reader

--- host/src/main.cpp
+++ host/src/main.cpp
@@ -1,11 +1,9 @@
-// TODO: Implementar aplicacion principal
+#include "processing/signal_processor.h"
 #include "serial/arduino_comm.h"
 #include "wireless/ble_client.h"
-#include "processing/signal_processor.h"
 
-int main(int argc, char* argv[]) {
-    (void)argc;
-    (void)argv;
-    // TODO
-    return 0;
+int main(int argc, char *argv[]) {
+
+
+  return 0;
 }
