d1b9ae0c28db0789f582dbadb99b612aabe74f7a
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-19 01:35:49 -0300

    fix: serial protocol mn

--- firmware/arduino-emg/src/serial_protocol.cpp
+++ firmware/arduino-emg/src/serial_protocol.cpp
@@ -15,9 +15,9 @@ serial_proto::Packet::Packet()
     : b_start{START_BYTE}, type{static_cast<uint8_t>(MessageType::emgAll)},
       b_size{sizeof(Payload)}, b_end{END_BYTE} {}
 
-serial_proto::Packet *const serial_proto::Packet::compute_checksum() {
+serial_proto::Packet &serial_proto::Packet::compute_checksum() {
   check_sum = payload.get_sum();
-  return this;
+  return *this;
 }
 
 size_t serial_proto::Packet::send() const {
@@ -30,7 +30,7 @@ int8_t serial_proto::send_emg_packet(const emg::Reader &reader) {
       .fill_payload([&reader](auto muscle, auto &buffer) {
         reader.get_data(muscle, buffer);
       })
-      ->compute_checksum()
-      ->send();
+      .compute_checksum()
+      .send();
   return SUCCESS;
 }
--- firmware/arduino-emg/src/serial_protocol.h
+++ firmware/arduino-emg/src/serial_protocol.h
@@ -5,14 +5,14 @@
 
 namespace serial_proto {
 
-#define SUCCESS 0
-
+constexpr const uint8_t SUCCESS = 0x00;
 constexpr const uint8_t START_BYTE = 0xAA;
 constexpr const uint8_t END_BYTE = 0x55;
 
-// TODO desacoplar
-constexpr const uint8_t single_muscle_payload_size = 32 * 2;
-constexpr const uint8_t max_payload_size = single_muscle_payload_size * 2;
+// This fixes the magic number problem but makes so that our protocol depends on
+// emg::Reaader, thats the tradeoff
+constexpr const uint8_t max_payload_size = emg::Reader::total_stream_size;
+constexpr const uint8_t single_muscle_payload_size = max_payload_size / 2;
 
 struct Payload {
   uint8_t leftBicep[single_muscle_payload_size];
@@ -31,16 +31,16 @@ struct Packet {
   const uint8_t b_end;
 
   Packet();
-  Packet *const compute_checksum();
-  template <typename Filler> Packet *const fill_payload(Filler filler_function);
+  Packet &compute_checksum();
+  template <typename Filler> Packet &fill_payload(Filler filler_function);
   size_t send() const;
 } __attribute__((packed));
 
 template <typename Filler>
-Packet *const Packet::fill_payload(Filler filler_function) {
+Packet &Packet::fill_payload(Filler filler_function) {
   filler_function(emg::Muscle::LeftBicep, this->payload.leftBicep);
   filler_function(emg::Muscle::RightBicep, this->payload.rightBicep);
-  return this;
+  return *this;
 }
 
 enum class MessageType : uint8_t {
