d2586be93f5deac4cf9c7a7aa8306a9c5e2bec41
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-20 17:41:04 -0300

    feat: working on signal processor

--- host/src/processing/signal_processor.cpp
+++ host/src/processing/signal_processor.cpp
@@ -1,2 +1,6 @@
-// TODO: Implementar
 #include "signal_processor.h"
+#include "serial/scsp.h"
+
+robrain::SignalProcessor::SignalProcessor(
+    serial::Consumer<serial_proto::Payload, 4064>& consumer)
+    : consumable_{consumer} {}
