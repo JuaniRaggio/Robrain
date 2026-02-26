4d624d3f1ec7ebdafb0b68c1cc0574f3f96b11b2
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-26 11:41:21 -0300

    minor

--- host/src/wireless/ble_client.h
+++ host/src/wireless/ble_client.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <simpleble/SimpleBLE.h>
 
 namespace robrain {
 
@@ -25,6 +26,18 @@ class BleClient {
 public:
     using StatusCallback = std::function<void(const RobotStatus&)>;
     using ConnectCallback = std::function<void(bool connected)>;
+
+    auto adapter = SimpleBLE::Adapter::get_adapters()[0];
+    SimpleBLE::Peripheral target;
+    bool found = false;
+
+    adapter.set_callback_on_scan_found([&](SimpleBLE::Peripheral peripheral) {
+        if (peripheral.identifier() == DEVICE_NAME) {
+            target = peripheral;
+            found = true;
+            adapter.scan_stop();
+        }
+    });
 
     BleClient();
     ~BleClient();
