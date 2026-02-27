2f820f3e49899245011c95326d098d7803334c72
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-27 13:26:38 -0300

    fix: const memory address just in case

--- common/protocol/wireless_packet.h
+++ common/protocol/wireless_packet.h
@@ -7,10 +7,10 @@
 namespace wireless_protocol {
 
 // Info compartida para configuracion host - esp32
-constexpr const char *DEVICE_NAME = "ROBRAIN_ROBOT";
-constexpr const char *SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-constexpr const char *CMD_CHAR_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-constexpr const char *STATUS_CHAR_UUID = "beb5483f-36e1-4688-b7f5-ea07361b26a8";
+constexpr const char * const DEVICE_NAME = "ROBRAIN_ROBOT";
+constexpr const char * const SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+constexpr const char * const CMD_CHAR_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+constexpr const char * const STATUS_CHAR_UUID = "beb5483f-36e1-4688-b7f5-ea07361b26a8";
 
 constexpr int16_t LIMIT_SPEED = 100;
 
