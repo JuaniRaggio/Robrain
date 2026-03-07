a65e2d39c52f7569570d9435beaa128e0defadcd
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-03-07 10:49:00 -0300

    fix: last_idx

--- firmware/arduino-emg/src/emg.cpp
+++ firmware/arduino-emg/src/emg.cpp
@@ -4,15 +4,15 @@
 // --- ChannelReader ---
 
 emg::Reader::ChannelReader::ChannelReader()
-    : last_idx{0}, pin{0}, stream_data{}, active{false} {}
+    : last_idx{-1}, pin{0}, stream_data{}, active{false} {}
 
 void emg::Reader::ChannelReader::read() {
-  stream_data[last_idx++] = analogRead(pin);
+  stream_data[++last_idx] = analogRead(pin);
   last_idx = last_idx % stream_size;
 }
 
 int8_t emg::Reader::ChannelReader::latest() const {
-  return stream_data[(last_idx + stream_size - 1) % stream_size];
+  return stream_data[last_idx];
 }
 
 bool emg::Reader::ChannelReader::is_full() const {
